cad642942a5ddf47b5fa4aec0b084d8774f23c69
andreatsilva <andre.emidio09@outlook.pt>
2024-10-11 02:40:51 +0100

    flapy bird game - machine learning

---
+++ Flapy Bird AI/main.py
@@ -0,0 +1,17 @@
+##Flapy Bird Game that uses AI 
+
+
+import pygame
+import random
+
+
+
+pygame.init()
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 700
+BIRD_WIDTH = 50
+BIRD_HEIGH = 50
+PIPE_WIDTH = 80
+PIPE_HEIGHT = 80
+PIPE_GAP  = 200 
